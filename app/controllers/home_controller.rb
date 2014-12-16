2b32ff064d183ab2a932b33f0de3865d69fa720c
sliwecki <sliweckid@gmail.com>
2014-12-16 14:01:58 +0100

    convert indentation to spaces

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,20 +6,20 @@ class HomeController < ApplicationController
   def new; end
   
   def create
-  	if @service.perform
-  		render text: @service.encrypted_url
-  	else
-  		render :new
-  	end
+    if @service.perform
+      render text: @service.encrypted_url
+    else
+      render :new
+    end
   end
 
   private
 
   def preload_form
-  	@form = UrlForm.new(params[:form_data])
+    @form = UrlForm.new(params[:form_data])
   end
 
   def preload_service
-  	@service = UrlSaver.new(@form)
+    @service = UrlSaver.new(@form)
   end
 end
