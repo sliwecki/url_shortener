d72ee2966a534d6e84137ed56107ce5328137658
sliwecki <sliweckid@gmail.com>
2014-12-16 14:04:04 +0100

    convert indentation to spaces

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 
 describe HomeController, type: :controller do
   describe 'GET new' do
-  	specify do
+    specify do
       get :new
-  	  expect(response).to have_http_status(:ok)
-  	end
+      expect(response).to have_http_status(:ok)
+    end
   end
 
   describe 'POST create' do
-  	specify do
-  	  post :create, form_data: { url: 'http://www.google.pl' }
-  	  expect(response).to have_http_status(:ok)
-  	end
+    specify do
+      post :create, form_data: { url: 'http://www.google.pl' }
+      expect(response).to have_http_status(:ok)
+    end
 
-  	specify do
-  	  post :create, form_data: { url: 'www.google.pl' }
-  	  expect(response).to render_template(:new)
-  	end
+    specify do
+      post :create, form_data: { url: 'www.google.pl' }
+      expect(response).to render_template(:new)
+    end
   end
 end
